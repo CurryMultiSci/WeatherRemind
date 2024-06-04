04cd0588a109232ca68a85b7cb15f5ecf12d60c7
CurryMultiSci <83395706+CurryMultiSci@users.noreply.github.com>
2024-06-04 13:38:43 +0800

    Update daily_sign.py

--- daily_sign.py
+++ daily_sign.py
@@ -7,7 +7,7 @@ url = os.environ.get("JD_URL")
 headers = {"Connection": 'keep-alive',
            "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
            "Cache-Control": 'no-cache',
-           "User-Agent": "jdapp;iPhone;12.4.3;;;M/5.0;appBuild/169159;",
+           "User-Agent": "okhttp/3.12.1;jdmall;android;version/10.3.4;build/92451;",
            "accept": "*/*",
            "connection": "Keep-Alive",
            "Accept-Encoding": "gzip, deflate, br",
